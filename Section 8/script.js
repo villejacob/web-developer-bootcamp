a1a4dc00687fe1eff26e0748fa64b108c5fe240d
Jacob Ville <VilleLJacob@gmail.com>
2016-11-24 17:35:09 -0500

    Add Section 8: Intro to Javascript

---
+++ Section 8/script.js
@@ -0,0 +1,5 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("... and your last name?");
+var age = prompt("How old are you " + firstName + "?");
+console.log("Your full name is " + firstName + " " + lastName + ".");
+console.log("You have encircled the sun " + age + " times.");
