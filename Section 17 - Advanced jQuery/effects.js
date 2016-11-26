d3b20145dc3c7d4b63f8f0e12898374763d0fe31
Jacob Ville <VilleLJacob@gmail.com>
2016-11-26 17:27:17 -0500

    Add Section 17: Advanced jQuery

---
+++ Section 17 - Advanced jQuery/effects.js
@@ -0,0 +1,14 @@
+$("button").on("click", function(){
+	// // Fade OUT on click
+	// $('div').fadeOut(1000, function(){
+	// 	$(this).remove();
+	// });
+	// // Fade IN on click
+	// $('div').fadeIn(1000);
+	// // Toggle display via fade
+	$("div").fadeToggle(500);
+});
+
+// $("button").on("click", function(){
+// 	$("div").slideToggle(500);
+// });
