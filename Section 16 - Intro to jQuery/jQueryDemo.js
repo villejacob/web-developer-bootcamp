e978a742e7569dbfe6af781997b5cafbcceba882
Jacob Ville <VilleLJacob@gmail.com>
2016-11-26 16:06:32 -0500

    Add Section 16: Intro to jQuery

---
+++ Section 16 - Intro to jQuery/jQueryDemo.js
@@ -0,0 +1,13 @@
+// 2. select all divs and give them a purple background
+$("div").css("background", "purple");
+
+// 3. select divs with class "highlight" and make them 200px wide
+$(".highlight").css("width", "200px");
+
+// 4. select the div with id "third" and give it an orange border
+$("#third").css("border", "4px solid orange");
+
+// 5. Select the first div only and change font color to pink
+$("div:first-of-type").css("color", "pink");
+// OR
+// $("div:first").css("color", "pink");
